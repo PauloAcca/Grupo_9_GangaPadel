5203c4987f278e640508a2cd52b981aaa1a4f497
PauloAcc <paulo.matias.accardo@gmail.com>
2023-02-15 20:08:39 -0300

    subo pagina de inicio de sesion

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require("express")
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.resolve(__dirname,"../public")))
+
 app.listen(3030, ()=> console.log("El server escucha en el puerto 3030"))
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/index.html"))
 })
 
-app.use(express.static(path.resolve(__dirname,"../public")))
+app.get("/inicioSesion",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/inicioSesion.html"))
+})
+
