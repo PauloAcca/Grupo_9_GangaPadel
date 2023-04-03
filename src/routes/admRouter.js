caa1ab7cd11bb46578715720e9478fa5ce15f83d
Seibox <seibomlove@gmail.com>
2023-04-03 09:54:34 -0300

    modificación adm router

--- src/routes/admRouter.js
+++ src/routes/admRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const admController =  require("../controllers/admController.js");
 
-router.get("/agregado", admController.agregado);
+router.get("adm/agregado", admController.agregado);
 
 module.exports = router;
