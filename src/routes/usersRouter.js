d3f776ea644984eb9d99dbd9228b782d9ff4f65c
ArielCesari <ariel.dcesari@gmail.com>
2023-04-24 20:10:59 -0300

    Creo rutas para perfil de usuario e invitado

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 const usersController =  require("../controllers/usersController.js");
+const authMiddleware = require("../middlewares/authMiddleware")
 router.get('/productCart',usersController.carrito);
-router.get('/wishlist',usersController.wishlist);
+router.get('/wishlist', authMiddleware,usersConbtroller.wishlist)
 module.exports = router;
