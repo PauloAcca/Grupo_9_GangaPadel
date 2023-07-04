6e08379d9464fa5df01589328dcd9bb5897c943a
Nazaa29 <lopeznazarenoo@gmail.com>
2023-07-04 15:42:44 -0300

    prueba

--- src/controllers/apiUsuarios.js
+++ src/controllers/apiUsuarios.js
@@ -1,37 +1,37 @@
-// let db = require("../../dataBase/models")
-// const Op = db.Sequelize.Op
+let db = require("../../dataBase/models")
+const Op = db.Sequelize.Op
 
 
-// const infoUsuarios = {
-//     info: (req,res)=>{
-//         db.Usuario
-//             .findAll()
-//             .then(usuarios => {
-//                 return res.json({
-//                     total:usuarios.length,
-//                     users: {
-//                         id: usuarios.id,
-//                         name: usuarios.nombre,
-//                         email: usuarios.email,
-//                         detail: ""
-//                     }
-//                 })
-//             })
-//     },
+const infoUsuarios = {
+    info: (req,res)=>{
+        db.Usuario
+            .findAll()
+            .then(usuarios => {
+                return res.json({
+                    total:usuarios.length,
+                    users: {
+                        id: usuarios.id,
+                        name: usuarios.nombre,
+                        email: usuarios.email,
+                        detail: ""
+                    }
+                })
+            })
+    },
 
-//     detalle: (req,res)=>{
-//         db.Usuario
-//             .findByPK(req.params.id)
-//             .then(usuario => {
-//                 return res.json({
-//                     id: usuario.id,
-//                     name: usuario.nombre,
-//                     apellido: usuario.apellido,
-//                     email: usuario.email,
-//                     imagen: ""
-//                 })
-//             })
-//     }
-// }
+    detalle: (req,res)=>{
+        db.Usuario
+            .findByPK(req.params.id)
+            .then(usuario => {
+                return res.json({
+                    id: usuario.id,
+                    name: usuario.nombre,
+                    apellido: usuario.apellido,
+                    email: usuario.email,
+                    imagen: ""
+                })
+            })
+    }
+}
 
-// module.exports = infoUsuarios;
+module.exports = infoUsuarios;
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,6 +3,40 @@ const router = express.Router();
 // Requerimos multer
 const multer = require('multer');
 const productsController =  require("../controllers/productsController.js");
+//API
+router.get("/api/products", (req, res) => {
+    db.Producto.findAll()
+        .then(products => res.json(products))
+        .catch(error => {
+            res.status(500).json({ error: 'Ha ocurrido un error, intente nuevamente' });
+        });
+});
+//API
+router.get("/api/category", (req, res) => {
+    db.Marca.findAll()
+        .then(categories => res.json(categories))
+        .catch(error => {
+            res.status(500).json({ error: 'Ha ocurrido un error, intente nuevamente' });
+        });
+});
+
+
+// api parametrizada
+router.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+
+    db.Producto.findByPk(productId)
+        .then(product => {
+            if (product) {
+                res.json(product);
+            } else {
+                res.status(404).json({ error: 'Producto no encontrado' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Ha ocurrido un error, intente nuevamente' });
+        });
+});
 router.get('/',productsController.filtrado);
 router.get('/detail/:id',productsController.detalle);
 router.get('/search',productsController.search);
